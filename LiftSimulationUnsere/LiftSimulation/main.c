3d44b77904e623585013d94b77702d708e6f8d04
levi-tox <t.fischer3@online.gibz.ch>
2019-12-09 11:57:58 +0100

    Project from Laich

--- LiftSimulationUnsere/LiftSimulation/main.c
+++ LiftSimulationUnsere/LiftSimulation/main.c
@@ -1,33 +1,50 @@
-/**
- * @file LiftSimulation.c
- * @brief Hauptprogramm und entry point der Liftsteuerung
+/*
+ * GccApplication1.c
  *
- * @date 20.11.2019 Erstellen des Templates
- * @author Epci Dev
+ * Created: 26.11.2019 16:29:04
+ * Author : rolfl
  */ 
 
-#include "LiftSimulationCommon.h"
+#include <avr/io.h>
+
 #include "LiftLibrary.h"
+#include "AppIncludes.h"
+
+
+
+
 
 void TestFunction(uint8_t* data, uint8_t nrOfBytes);
+void FindBit(char ch);
 
 int main(void)
 {
-    /* Replace with your application code */
-   InitializePorts();
-   Usart_Init();
-   RegisterTestHandler( TestFunction );
-   InitializeStart();
+    InitializePorts();
+	Usart_Init();
+	RegisterTestHandler( TestFunction );
+	RegisterFsm(&_motorCtrl.fsm);
+	// letzte Funktion die aufgerufen wird!
+	InitializeStart();
 }
 
 void TestFunction(uint8_t* data, uint8_t nrOfBytes)
 {
-	if ( data[0] == 1 )
+	if( data[0] == 1 )
 	{
-		char *ch = "Hello World\n";
-		while ( *ch != 0 )
+		char *ch = "Hello-World\n";
+		while( *ch != 0 )
 		{
 			Usart_PutChar(*ch++);
-		} 
+		}
+	}
+	else if( data[0] == 2 )
+	{
+		SendEvent(SignalSourceApp, Message_MoveTo, data[1], 0);
 	}
 }
+
+int FindBit(char ch){
+	
+	return (0x72f588 >> ((((ch * 29) >> 5) & 0x7) * 3)) & 0x7;
+}
+
